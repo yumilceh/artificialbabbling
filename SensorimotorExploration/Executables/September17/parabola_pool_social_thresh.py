d58e3ab8c8ad36e29f7d2c6c9d77c8607d4c4872
yumilceh <jmavbpl@gmail.com>
2017-10-02 17:33:56 +0200

    Parabola final experiments
    Former-commit-id: c0fbb97e277591c5749dc44121670614b516191b

--- SensorimotorExploration/Executables/September17/parabola_pool_social_thresh.py
+++ SensorimotorExploration/Executables/September17/parabola_pool_social_thresh.py
@@ -7,6 +7,7 @@ import datetime
 import itertools
 
 if __name__ == '__main__':
+
     #  Adding the projects folder to the path##
     import os
 
@@ -41,97 +42,100 @@ if __name__ == '__main__':
 
     # random.seed(random_seed)
     # np_rnd.seed(random_seed)
-    directory = 'parabola_slope_social_thresh_8'
+    directory = 'parabola_slope_social_thresh_final_1'
     os.mkdir(directory)
 
-    #
-    random_seeds = [8975, 91324,752324,1264183, 82376, 92835, 823975,147831, 234096, 2453, 2340554, 1234, 1321, 1457, 283, 2469,  12455,  2376324,
-                    879363, 248979,43087926,564642,256874,344134,434634,34564,534645,344655,36455,31256]
-    mode_ops = ['autonomous','social']
-    social_slopes = [1, 0.995]
+    # random_seeds = [8975, 91324,752324,1264183, 82376, 92835, 823975,147831, 234096, 2453, 2340554, 1234, 1321, 1457, 283, 2469,  12455,  2376324,
+    #                 879363, 248979,43087926,564642,256874,344134,434634,34564,534645,344655,36455,31256]
+    random_seeds = range(100)
+    mode_ops = ['social']
+    social_slopes = [1., 0.995, 0.99, 0.985, 0.98]
 
-    for idx,ops in enumerate(itertools.product(random_seeds, mode_ops, social_slopes)):
-        # Creating Agent ##
-        system = System()
-        if ops[1] is 'social':
-            instructor = Instructor(thresh_slope=ops[2])
-        else:
-            instructor = None
+    groups1 = itertools.product(random_seeds, mode_ops, social_slopes)
+    groups2 = itertools.product(random_seeds, ['autonomous'], [1.])
+    for group in [groups1, groups2]:
+        for idx,ops in enumerate(group):
+            # Creating Agent ##
+            system = System()
+            if ops[1] is 'social':
+                instructor = Instructor(thresh_slope=ops[2])
+            else:
+                instructor = None
 
-        #interest model selection
-        f_im_key = 'explauto_im'
+            #interest model selection
+            f_im_key = 'explauto_im'
 
-        random_seed = ops[0]
-        mode_ = ops[1]
+            random_seed = ops[0]
+            mode_ = ops[1]
 
-        # Creating Models ##
-        models = OBJECT()
+            # Creating Models ##
+            models = OBJECT()
 
-        models.f_sm = model_(f_sm_key, system)
-        models.f_ss = model_(f_ss_key, system)
-        models.f_im = model_(f_im_key, system)
-        models.f_cons = model_(f_cons_key, system)
+            models.f_sm = model_(f_sm_key, system)
+            models.f_ss = model_(f_ss_key, system)
+            models.f_im = model_(f_im_key, system)
+            models.f_cons = model_(f_cons_key, system)
 
-        now = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S_")
-        file_prefix = directory + '/Parabola_Pool_' + str(idx) + '_' + now
+            now = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S_")
+            file_prefix = directory + '/Parabola_Pool_' + str(idx) + '_' + now
 
-        #  Creating Simulation object, running simulation and plotting experiments##
-        evaluation_sim = SM_ModelEvaluation(system,
-                                            models.f_sm, comp_func=comp_func)
+            #  Creating Simulation object, running simulation and plotting experiments##
+            evaluation_sim = SM_ModelEvaluation(system,
+                                                models.f_sm, comp_func=comp_func)
 
-        evaluation_sim.load_eval_dataset('../../Systems/datasets/parabola_v2_dataset.h5')
+            evaluation_sim.load_eval_dataset('../../Systems/datasets/parabola_v2_dataset.h5')
 
-        simulation = Algorithm(system,
-                               models,
-                               n_experiments,
-                               comp_func,
-                               instructor = instructor,
-                               n_initialization_experiments=n_initialization,
-                               random_seed=random_seed,
-                               evaluation=evaluation_sim,
-                               eval_step=eval_step,
-                               g_im_initialization_method='all', #'all'
-                               n_save_data=n_save_data,
-                               sm_all_samples=False,
-                               file_prefix=file_prefix)
-        simulation.mode = mode_ # social or autonomous
+            simulation = Algorithm(system,
+                                   models,
+                                   n_experiments,
+                                   comp_func,
+                                   instructor = instructor,
+                                   n_initialization_experiments=n_initialization,
+                                   random_seed=random_seed,
+                                   evaluation=evaluation_sim,
+                                   eval_step=eval_step,
+                                   g_im_initialization_method='all', #'all'
+                                   n_save_data=n_save_data,
+                                   sm_all_samples=False,
+                                   file_prefix=file_prefix)
+            simulation.mode = mode_ # social or autonomous
 
-        simulation.f_sm_key = f_sm_key
-        simulation.f_ss_key = f_ss_key
-        simulation.f_cons_key = f_cons_key
-        simulation.f_im_key = f_im_key
+            simulation.f_sm_key = f_sm_key
+            simulation.f_ss_key = f_ss_key
+            simulation.f_cons_key = f_cons_key
+            simulation.f_im_key = f_im_key
 
-        simulation.run(proprio=True)
+            simulation.run(proprio=True)
 
-        #evaluate sensorimotor
-        evaluation_sim = SM_ModelEvaluation(system,
-                                            simulation.models.f_sm,
-                                            comp_func=comp_func,
-                                            file_prefix=file_prefix + 'eval_whole_')
+            #evaluate sensorimotor
+            evaluation_sim = SM_ModelEvaluation(system,
+                                                simulation.models.f_sm,
+                                                comp_func=comp_func,
+                                                file_prefix=file_prefix + 'eval_whole_')
 
-        # evaluate
-        evaluation_sim.model.set_sigma_explo_ratio(0.)
-        evaluation_sim.model.mode = 'exploit'
+            # evaluate
+            evaluation_sim.model.set_sigma_explo_ratio(0.)
+            evaluation_sim.model.mode = 'exploit'
 
-        evaluation_sim.load_eval_dataset('../../Systems/datasets/parabola_v2_dataset.h5')
-        evaluation_sim.model.set_sigma_explo_ratio(0.)
-        val_data = evaluation_sim.evaluate(space='sensor', saveData=True)
+            evaluation_sim.load_eval_dataset('../../Systems/datasets/parabola_v2_dataset.h5')
+            evaluation_sim.model.set_sigma_explo_ratio(0.)
+            val_data = evaluation_sim.evaluate(space='sensor', saveData=True)
 
-        evaluation_sim = SM_ModelEvaluation(system,
-                                            simulation.models.f_sm,
-                                            comp_func=comp_func,
-                                            file_prefix=file_prefix + 'eval_social_')
+            evaluation_sim = SM_ModelEvaluation(system,
+                                                simulation.models.f_sm,
+                                                comp_func=comp_func,
+                                                file_prefix=file_prefix + 'eval_social_')
 
-        # evaluate
-        evaluation_sim.model.set_sigma_explo_ratio(0.)
-        evaluation_sim.model.mode = 'exploit'
+            # evaluate
+            evaluation_sim.model.set_sigma_explo_ratio(0.)
+            evaluation_sim.model.mode = 'exploit'
 
-        evaluation_sim.load_eval_dataset('../../Systems/datasets/instructor_parabola_1.h5')
-        evaluation_sim.model.set_sigma_explo_ratio(0.)
-        val_data = evaluation_sim.evaluate(space='sensor', saveData=True)
+            evaluation_sim.load_eval_dataset('../../Systems/datasets/instructor_parabola_1.h5')
+            evaluation_sim.model.set_sigma_explo_ratio(0.)
+            val_data = evaluation_sim.evaluate(space='sensor', saveData=True)
 
-        del simulation
-        del models
-        del evaluation_sim
+            del simulation
+            del models
+            del evaluation_sim
 
     print('FINITO')
