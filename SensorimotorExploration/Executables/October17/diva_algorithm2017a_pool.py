197af67e8aef6bbc28be00d0dbe53cf4ef769b7c
yumilceh <jmavbpl@gmail.com>
2017-10-28 02:04:11 +0100

    Last simulations
    Former-commit-id: a0863ec6850d1f2fc71119664515f7b4003e2e71

--- SensorimotorExploration/Executables/October17/diva_algorithm2017a_pool.py
+++ SensorimotorExploration/Executables/October17/diva_algorithm2017a_pool.py
@@ -17,7 +17,7 @@ from SensorimotorExploration.Systems.Diva2017a import Diva2017a as System
 from SensorimotorExploration.Systems.Diva2017a import Instructor
 from diva_configurations import model_, comp_func
 
-directory = 'experiment_IEEE_SI_slopes_cmf_123'
+directory = 'diva_IEEE_SI/experiment_IEEE_SI_slopes_cmf_050'
 
 # Models
 f_sm_key = 'igmm_sm'
@@ -110,8 +110,8 @@ if __name__ == '__main__':
     # 2469, 147831, 1234
     random_seeds = [1321,1457, 283, 2469, 147831, 1234]
     mode_ops = ['social']
-    social_slopes = [1., 0.999999]# [1., 0.999999, 0.99, 0.96, 0.93]
-    vowel_units = [223]#323,223,123,50]
+    social_slopes = [0.93]# [1., 0.999999, 0.99, 0.96, 0.93]
+    vowel_units = [50]#323,223,123,50]
 
     groups1 = itertools.product(random_seeds, mode_ops, social_slopes, vowel_units)
     groups2 = itertools.product(random_seeds, ['autonomous'], [0.999999],vowel_units)
@@ -119,7 +119,7 @@ if __name__ == '__main__':
     processes = []
     max_processes = 3
 
-    for idx, ops in enumerate(list(groups1)+list(groups2)):
+    for idx, ops in enumerate(list(groups1)):#+list(groups2)):
         idx2 = idx
         # Creating Agent ##
         processes += [Process(target=sim_agent, args=(ops,idx2))]
